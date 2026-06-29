0f8c2ebb9698b7f352dfe3cfae68e46ac7f5beb6
Rapeepat <frk24072561@gmail.com>
2026-06-29 16:16:41 +0700

    สร้าง route view

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>บทความ</title>
+</head>
+<body>
+    <h2>บทความทั้งหมด</h2>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis in dolor placeat labore. Quis ipsam voluptates molestiae sunt ea! Velit eveniet corporis corrupti eum esse, laborum perspiciatis est maxime suscipit vero doloremque distinctio, earum atque labore nostrum dicta ab odio. Quis corrupti quos maiores repellendus molestiae incidunt quo quod libero cum veritatis voluptate est, quae a, totam, dolores culpa. Hic assumenda quos, totam voluptatem perspiciatis odio blanditiis enim, libero culpa nulla repudiandae a provident! Delectus iure ad obcaecati illum dolores architecto aspernatur ab voluptas, quia voluptatem reprehenderit maxime officiis amet, laborum fugiat et in aliquid veniam. Expedita inventore excepturi pariatur.</p>
+    <a style="margin: 20px; font-size: 30px;" href="/">หน้าแรก</a>
+</body>
+</html>
